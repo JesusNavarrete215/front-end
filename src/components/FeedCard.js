5f56702f351dfbb2080a254dbb19c019bf48e360
KevinC555 <kaseycorcoran95@bellsouth.net>
2021-08-25 12:55:56 -0500

    feed-card-styling

--- src/components/FeedCard.js
+++ src/components/FeedCard.js
@@ -6,11 +6,11 @@ export default function FeedCard(props) {
 	return (
 		<div>
 			<div className="feed-card">
-				<div>First name: {firstName}</div>
-				<div>Last name: {lastName}</div>
-				<div>Date: {date}</div>
-				<div>Location: {location}</div>
-				<div>Theme: {theme}</div>
+				<div><img className="card-img" src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" /></div>
+				<div class="name">{firstName} {lastName}</div>
+				<div class="date">{date}</div>
+				<div class="location">{location}</div>
+				<div class="theme">{theme}</div>
 			</div>
 		</div>
 	)
